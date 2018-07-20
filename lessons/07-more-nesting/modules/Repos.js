52551efebef798be678d2aa85a8a7bdc6756bff3
313183373 <313183373@qq.com>
2018-07-20 09:53:26 +0800

    Repo嵌套进Repos

--- lessons/07-more-nesting/modules/Repos.js
+++ lessons/07-more-nesting/modules/Repos.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
 
 export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h2>Repos</h2>
-        <ul>
-          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
-          <li><Link to="/repos/facebook/react">React</Link></li>
-        </ul>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>Repos</h2>
+                <ul>
+                    <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+                    <li><Link to="/repos/facebook/react">React</Link></li>
+                    {this.props.children}
+                </ul>
+            </div>
+        )
+    }
 })
