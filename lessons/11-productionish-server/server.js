09da487d8701078f5b8d82c3150fa47e8810deef
313183373 <313183373@qq.com>
2018-07-20 14:22:19 +0800

    添加Express if-env compression包，添加生产环境的服务器

---
+++ lessons/11-productionish-server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname));
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, function () {
+    console.log('Production Express server running at localhost:' + PORT);
+});
